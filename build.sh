1277d286f605a7fdd4b88f0bea6ac1027835dc44
Marcelo Coraça de Freitas <marcelo@kow.com.br>
2007-02-03 22:35:02 +0000

    added project path of xmlada to build.sh but it's still not working..
    git-svn-id: http://dev.adaworks.net/svn/trunk/adaconfig@25 115cb82a-aa27-0410-86a0-a2651309cc67

--- build.sh
+++ build.sh
@@ -3,11 +3,19 @@
 # This script calls gnatmake using the adaconfig.gpr project file.
 
 
+
+# Add other AdaWorks projects to the path:
 ADA_PROJECT_PATH=.
 for i in ../*;
 do
 	ADA_PROJECT_PATH=${ADA_PROJECT_PATH}:$i;
 done
+
+# Add XML/Ada to the path:
+ADA_PROJECT_PATH="${ADA_PROJECT_PATH}:`xmlada-config --libs | cut -d "L" -f2 | cut -d " " -f1`/gnat"
+
+echo $ADA_PROJECT_PATH
+
 
 
 echo $ADA_PROJECT_PATH
