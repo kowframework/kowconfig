4c8c4695130109929b466ddbff79b3afc169725e
Marcelo Coraça de Freitas <marcelo@kow.com.br>
2007-02-08 16:28:25 +0000

    added samples of adaconfig
    git-svn-id: http://dev.adaworks.net/svn/trunk/adaconfig@50 115cb82a-aa27-0410-86a0-a2651309cc67

---
+++ samples/sample_project.adb
@@ -0,0 +1,16 @@
+-- Sample project for AdaConfig.
+--
+-- @author Marcelo Coraça de Freitas <marcelo.batera@gmail.com>
+-- @createdAt 2007-01-30
+-- @lastUpdate 
+
+
+
+with Ada.Text_IO;	use Ada.Text_IO;
+
+with Ada_Config;	use Ada_Config;
+
+procedure Sample_Project is
+begin
+	Put_Line( "Testing" );
+end Sample_Project;
