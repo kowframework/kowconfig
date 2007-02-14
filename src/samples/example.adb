5e3850c2ce30f08849b5a6c28502dd806fc783b0
Marcelo Coraça de Freitas <marcelo@kow.com.br>
2007-02-14 22:04:46 +0000

    adding example to repository
    git-svn-id: http://dev.adaworks.net/svn/trunk/awconfig@93 115cb82a-aa27-0410-86a0-a2651309cc67

---
+++ src/samples/example.adb
@@ -0,0 +1,49 @@
+
+
+
+with Aw_Config;			use Aw_Config;
+with Ada.Text_IO;		use Ada.Text_IO;
+with Ada.Strings.Unbounded;	use Ada.Strings.Unbounded;
+
+package body Example is
+
+	procedure Run_Example( P: in Parser_Access ) is
+		-- run a test
+	begin
+
+		Add_Config_Path( "./data" );
+
+		Put_Line( "Loading config file... " );
+		Config := New_Config_File( "config", P );
+
+
+		Put_Line( "Fetching information" );
+
+		New_Line;
+
+		Put_Line( "Section => ""Main Section""" );
+		Set_Section( Config, "Main Section" );
+		Put_Line( "     company name     => " & To_String( Element( Config, "company name" ) ) );
+		Put_Line( "     company size     => " & To_String( Element( Config, "company size" ) ) );
+
+		New_Line;
+
+		Put_Line( "Section => ""Main Section.Contact Information""" );
+		Set_Section( Config, "Main Section.Contact Information" );
+		Put_Line( "     address          => " & To_String( Element( Config, "address" ) ) );
+
+		New_Line;
+
+		Put_Line( "Section => ""Database"" " );
+		Set_Section( Config, "Database" );
+		Put_Line( "     host     => " & To_String( Element( Config, "host" ) ) );
+		Put_Line( "     user     => " & To_String( Element( Config, "user" ) ) );
+		Put_Line( "     password => " & To_String( Element( Config, "password" ) ) );
+		Put_Line( "     database => " & To_String( Element( Config, "database" ) ) );
+		Put_Line( "     engine   => " & To_String( Element( Config, "engine" ) ) );
+
+
+		New_Line;
+		Put_Line( "If you didn't see any error message untill now it should be running fine" );
+	end Run_Example;
+end Example;
---
+++ src/samples/xml_test.adb
@@ -0,0 +1,10 @@
+
+
+
+with Aw_Config.Xml_Parsers;	use Aw_Config.Xml_Parsers;
+with Example;			use Example;
+
+procedure Xml_Test is
+begin
+	Run_Example( new Parser );
+end Xml_Test;
