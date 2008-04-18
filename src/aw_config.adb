f17c0d3aab3c7c6e18dd59275f4ad305b9ff7312
Adele Helena Ribeiro <adele.helena@gmail.com>
2008-04-18 20:35:21 +0000

    Fixed Identation and commented Aw_Config, Aw_Lib-Samples and sorces for Linux and Windows of the Aw_Lib.
    git-svn-id: http://dev.adaworks.net/svn/trunk/awconfig@338 115cb82a-aa27-0410-86a0-a2651309cc67

--- src/aw_config.adb
+++ src/aw_config.adb
@@ -1,23 +1,47 @@
--- This is the main AdaConfig package.
--- Here you'll find the types you should use in your application and all 
--- visible procedures and functions.
---
--- author Marcelo C. de Freitas <marcelo.batera@gmail.com>
--- createdAt 2007-01-25
---
--- Repository information:
--- $Date$
--- $Revision$
--- $Author$
+------------------------------------------------------------------------------
+--                                                                          --
+--                          Ada Works :: Library                            --
+--                                                                          --
+--                                Ada Works                                 --
+--                                                                          --
+--                                 B o d y                                  --
+--                                                                          --
+--         Copyright (C) 2007-2008, Ydea Desenv. de Softwares Ltda          --
+--                                                                          --
+--                                                                          --
+-- Aw_Lib is free library;  you can redistribute it  and/or modify it under --
+-- terms of the  GNU General Public License as published  by the Free Soft- --
+-- ware  Foundation;  either version 2,  or (at your option) any later ver- --
+-- sion. Aw_Lib is distributed in the hope that it will be useful, but WITH---
+-- OUT ANY WARRANTY;  without even the  implied warranty of MERCHANTABILITY --
+-- or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License --
+-- for  more details.  You should have  received  a copy of the GNU General --
+-- Public License  distributed with Aw_Lib; see file COPYING. If not, write --
+-- to  the Free Software Foundation,  59 Temple Place - Suite 330,  Boston, --
+-- MA 02111-1307, USA.                                                      --
+--                                                                          --
+-- As a special exception,  if other files  instantiate  generics from this --
+-- unit, or you link  this unit with other files  to produce an executable, --
+-- this  unit  does not  by itself cause  the resulting  executable  to  be --
+-- covered  by the  GNU  General  Public  License.  This exception does not --
+-- however invalidate  any other reasons why  the executable file  might be --
+-- covered by the  GNU Public License.                                      --
+------------------------------------------------------------------------------
 
-
+------------------------------------------------------------------------------
+-- This is the Aw_Config package                                            --
+--                                                                          --
+-- This is the main AdaConfig package.                                      --
+-- Here you'll find the types you should use in your application and all    -- 
+-- visible procedures and functions.                                        --
+------------------------------------------------------------------------------
 
 
 with Ada.Containers.Ordered_Maps;
 with Ada.Directories;
 with Ada.Environment_Variables;
 with Ada.Exceptions;
-with Ada.Strings.Unbounded;	use Ada.Strings.Unbounded;
+with Ada.Strings.Unbounded;		use Ada.Strings.Unbounded;
 
 with Aw_Lib.File_System;
 with Aw_Lib.String_Util;
